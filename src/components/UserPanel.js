8c9d406ed5ed009b095394116b8944873e8cc051
Bartłomiej Janusz <bartek.m.janusz@gmail.com>
2022-10-27 21:32:36 +0200

    calories added

--- src/components/UserPanel.js
+++ src/components/UserPanel.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const options = [
-    { id: 1, value: 'chicken', protein: 27 },
-    { id: 2, value: 'eggs', protein: 13 },
-    { id: 3, value: 'cheese piątnica semi-fat', protein: 16 },
-    { id: 4, value: 'peanut butter', protein: 23.78 }
+    { id: 1, value: 'chicken', protein: 27, calories: 239 },
+    { id: 2, value: 'eggs', protein: 13, calories: 155.1 },
+    { id: 3, value: 'cheese piątnica semi-fat', protein: 16, calories: 485 },
+    { id: 4, value: 'peanut butter', protein: 23.78, calories: 642 }
 ]
 
 class UserPanel extends React.Component {
@@ -15,6 +15,8 @@ class UserPanel extends React.Component {
             weight: 0,
             protein: 0,
             total: 0,
+            totalKcal: 0,
+            calories: 0,
             id: 0,
             name: '',
             foods: []
@@ -22,11 +24,13 @@ class UserPanel extends React.Component {
         this.handleWeightChange = this.handleWeightChange.bind(this);
         this.handleProteinChange = this.handleProteinChange.bind(this);
         this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleCaloriesChange = this.handleCaloriesChange.bind(this);
     }
 
     addObjectToArray = e => {
         e.preventDefault();
         let proteinPerFood = (this.state.weight * this.state.protein) / 100
+        let caloriesPerFood = (this.state.weight * this.state.calories) / 100
         this.setState({
             foods: [
                 ...this.state.foods,
@@ -36,7 +40,8 @@ class UserPanel extends React.Component {
             protein: 0,
             weight: 0,
             id: this.state.id + 1,
-            total: this.state.total + proteinPerFood
+            total: this.state.total + proteinPerFood,
+            totalKcal: this.state.totalKcal + caloriesPerFood
         })
     };
 
@@ -52,6 +57,12 @@ class UserPanel extends React.Component {
         });
     }
 
+    handleCaloriesChange(e) {
+        this.setState({
+            calories: e.target.value
+        });
+    }
+
     handleNameChange(e) {
         let n = e.target.value
         this.setState({
@@ -60,14 +71,16 @@ class UserPanel extends React.Component {
         let obj = options.find(o => o.value === n);
         if (obj) {
             this.setState({
-                protein: obj.protein
+                protein: obj.protein,
+                calories: obj.calories
             });
         }
     }
 
     render() {
         return <>
-            <h2>Total Protein {Math.round(this.state.total * 100) / 100}g</h2>
+            <h3>Total calories {Math.round(this.state.totalKcal * 100) / 100}kcal</h3>
+            <h3>Total protein {Math.round(this.state.total * 100) / 100}g</h3>
 
             <label>food:</label>
             <input
@@ -94,6 +107,11 @@ class UserPanel extends React.Component {
                 name="protein"
                 value={this.state.protein}
                 onChange={this.handleProteinChange} />
+            <label>calories</label>
+            <input
+                name="calories"
+                value={this.state.calories}
+                onChange={this.handleCaloriesChange} />
             {this.state.foods.map(food => {
                 return (
                     <div key={food.id}>
