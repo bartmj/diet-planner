5d6ae77d4d1f8b52234a3586404bb26cf757126e
Bartłomiej Janusz <bartek.m.janusz@gmail.com>
2022-10-28 17:06:19 +0200

    meta tag

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import UserPanel from './components/UserPanel';
+import MetaTags from 'react-meta-tags';
 
 function App() {
 
@@ -10,6 +11,10 @@ function App() {
 
   return (
     <div className="App">
+      <MetaTags>
+        <title>Diet Planner - Project</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
+      </MetaTags>
       <header className="App-header">
         <div className="container">
           <h1>Diet Planner</h1>
